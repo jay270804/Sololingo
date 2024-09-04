467e32061890763474959f691a30cd5f8a691536
jay270804 <jaypatel270804@gmail.com>
2024-09-05 02:45:34 +0530

    Added Feed and Side Wrapper to /learn screen.

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,9 +1,24 @@
-const learnPage=()=>{
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { Header } from "./header";
+import { UserProgress } from "@/components/user-progress";
+
+const LearnPage=()=>{
     return(
-        <>
-        <p>This is learning page...</p>
-        </>
+        <div className="flex flex-row-reverse gap-[48px] px-6 ">
+            <StickyWrapper>
+                <UserProgress 
+                    activeCourse={{title:"Japanese", imageSrc:"/jp.svg"}}
+                    hearts={5}
+                    points={100}
+                    hasActiveSubscription={false}
+                />
+            </StickyWrapper>
+            <FeedWrapper>
+                <Header title="Japanese"/>
+            </FeedWrapper>
+        </div>
     )
 }
 
-export default learnPage;
+export default LearnPage;
