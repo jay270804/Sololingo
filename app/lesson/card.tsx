dbc18182906965632ac61b8b0198ca0b1ccd6af5
jay270804 <jaypatel270804@gmail.com>
2024-09-19 00:47:48 +0530

    challenge-progress action added to update userProgress and challengeProgress, added audio and keys (footer)

--- app/lesson/card.tsx
+++ app/lesson/card.tsx
@@ -1,3 +1,5 @@
+import { useCallback } from "react"
+import { useAudio, useKey } from "react-use"
 import { challenges } from "@/db/schema"
 import { cn } from "@/lib/utils"
 import Image from "next/image"
@@ -27,8 +29,20 @@ export const Card = ({
     status,
     type,
 }: Props) => {
+
+    const [audio, _, controls] = useAudio({ src: audioSrc || "" })
+
+    const handleClick = useCallback(() => {
+        if (disabled) return;
+
+        controls.play();
+        onClick();
+    }, [disabled, onClick, controls]);
+
+    useKey(shortcut, handleClick, {}, [handleClick])
+
     return (
-        <div onClick={() => { }}
+        <div onClick={handleClick}
             className={cn(
                 "h-full border-2 rounded-xl border-b-4 hover:bg-black/5 p-4 lg:p-6 cursor-pointer active:border-b-2",
                 selected && "border-sky-300 bg-sky-100 hover:bg-sky-100",
@@ -37,6 +51,7 @@ export const Card = ({
                 disabled && "pointer-events-none hover:bg-white",
                 type === "ASSIST" && "lg:p-3 w-full"
             )}>
+            {audio}
             {imageSrc && (
                 <div className="relative aspect-square mb-4 max-h-[80px] lg:max-h-[150px] w-full">
                     <Image
@@ -52,7 +67,7 @@ export const Card = ({
             )}>
                 {type === "ASSIST" && <div />}
                 <p className={cn(
-                    "text-neutral-600 text-sm lg:text-base",
+                    "text-neutral-600 text-sm lg:text-base font-semibold",
                     selected && "text-sky-500",
                     selected && status === "correct" && "text-green-500",
                     selected && status === "wrong" && "text-rose-500",
--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -1,10 +1,13 @@
 'use client'
 
 import { challengeOptions, challenges } from "@/db/schema"
-import { useState } from "react"
+import { useState, useTransition } from "react"
 import { Header } from "./header"
 import { QuestionBubble } from "./question-bubble"
 import { Challenge } from "./challenge"
+import { Footer } from "./footer"
+import { upsertChallengeProgress } from "@/actions/challenge-progress"
+import { toast } from "sonner"
 
 type Props = {
     initialPercentage: number,
@@ -24,6 +27,7 @@ export const Quiz = ({
     initialLessonId,
     userSubscription
 }: Props) => {
+    const [isPending, startTransition] = useTransition()
     const [hearts, setHearts] = useState(initialHearts)
     const [percentage, setPercentage] = useState(initialPercentage)
     const [challenges] = useState(initialLessonChallenges)
@@ -32,8 +36,65 @@ export const Quiz = ({
         return uncompletedIndex === -1 ? 0 : uncompletedIndex;
     })
 
+    const [selectedOption, setSelectedOption] = useState<number>();
+    const [status, setStatus] = useState<"correct" | "wrong" | "none">("none");
+
     const currentChallenge = challenges[activeIndex]
     const options = currentChallenge?.challengeOptions ?? [];
+
+    const onNext = () => {
+        setActiveIndex((currentIndex) => currentIndex + 1)
+    }
+
+    const onSelect = (id: number) => {
+        if (status !== "none") return;
+
+        setSelectedOption(id);
+    }
+
+    const onContinue = () => {
+        if (!selectedOption) return;
+
+        if (status === "wrong") {
+            setStatus("none")
+            setSelectedOption(undefined)
+            return;
+        }
+        if (status === "correct") {
+            onNext()
+            setStatus("none")
+            setSelectedOption(undefined)
+            return;
+        }
+
+        const correctOption = options.find((option) => option.correct)
+
+        if (!correctOption) {
+            return;
+        }
+
+        if (correctOption && correctOption.id === selectedOption) {
+            startTransition(() => {
+                upsertChallengeProgress(currentChallenge.id)
+                    .then((response) => {
+                        if (response?.error === 'hearts') {
+                            console.error("Missing hearts");
+                            return;
+                        }
+
+                        setStatus("correct");
+                        setPercentage((prev) => prev + 100 / challenges.length);
+
+                        if (initialPercentage === 100) {
+                            setHearts((prev) => Math.min(prev + 1, 5))
+                        }
+                    })
+                    .catch((err)=>toast.error("Something went wrong! Please try again."))
+            })
+        } else {
+            console.error("incorrect option")
+        }
+    }
 
     const title = currentChallenge.type === 'SELECT' ? currentChallenge.question : "Select the Correct meaning.";
     return (
@@ -55,9 +116,9 @@ export const Quiz = ({
                             }
                             <Challenge
                                 options={options}
-                                onSelect={()=>{}}
-                                status="none"
-                                selectedOption={undefined}
+                                onSelect={onSelect}
+                                status={status}
+                                selectedOption={selectedOption}
                                 disabled={false}
                                 type={currentChallenge.type}
                             />
@@ -65,6 +126,11 @@ export const Quiz = ({
                     </div>
                 </div>
             </div>
+            <Footer
+                disabled={!selectedOption}
+                status={status}
+                onCheck={onContinue}
+            />
         </>
     )
 }
